ac0d10d22b16a580b1d73270d2974a105910d4a2
TK <leandrotk100@gmail.com>
2019-07-08 16:46:50 -0300

    Implement get http request with promises

---
+++ get/index.js
@@ -0,0 +1,42 @@
+const get = (url) => {
+  const request = new XMLHttpRequest();
+
+  return new Promise((resolve, reject) => {
+    request.onreadystatechange = () => {
+      if (request.readyState !== 4) return;
+
+      if (request.status >= 200 && request.status < 300) {
+        resolve(request);
+      } else {
+        reject({
+          status: request.status,
+          statusText: request.statusText
+        });
+      }
+    };
+
+    request.onerror = () => {
+      reject();
+    };
+
+    request.open('GET', url, true);
+    request.send();
+  });
+}
+
+const jsonParse = (request) => JSON.parse(request.response);
+
+const template = ({ name }) => `<li>${name}</li>`;
+
+const renderPokemons = (pokemons) => {
+  const list = document.querySelector('#pokemon-list')
+  list.innerHTML = pokemons
+    .results
+    .map(template)
+    .join(' ');
+};
+
+get('https://pokeapi.co/api/v2/pokemon')
+  .then(jsonParse)
+  .then(renderPokemons);
+
